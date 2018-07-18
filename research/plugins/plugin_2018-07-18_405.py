8cfb6c1278141ac0f81b03393b30d9918ade3409
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2018-07-18 16:31:00 +0530

    iot research: publish_retry_backoff mqtt python backend (2018-07-18)

---
+++ research/plugins/plugin_2018-07-18_405.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: publish_retry_backoff (2018-07-18)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20180718_405 = MQTTPluginHandler("publish_retry_backoff", lambda p: p)
