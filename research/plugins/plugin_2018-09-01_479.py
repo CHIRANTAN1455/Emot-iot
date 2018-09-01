6fdc55bf7d085e5f1dbea4f04f2e691a65f5f2d0
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2018-09-01 17:23:00 +0530

    iot research: mqtt_sn_gateway_research mqtt python backend (2018-09-01)

---
+++ research/plugins/plugin_2018-09-01_479.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: mqtt_sn_gateway_research (2018-09-01)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20180901_479 = MQTTPluginHandler("mqtt_sn_gateway_research", lambda p: p)
