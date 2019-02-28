37341f0f56d26faae42713341b72de48f62120a9
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2019-02-28 19:51:00 +0530

    iot research: broker_load_balancing mqtt python backend (2019-02-28)

---
+++ research/plugins/plugin_2019-02-28_774.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: broker_load_balancing (2019-02-28)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20190228_774 = MQTTPluginHandler("broker_load_balancing", lambda p: p)
