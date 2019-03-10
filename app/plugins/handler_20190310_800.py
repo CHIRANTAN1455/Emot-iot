063e71892eab6586b65f86dae9728b8de954ab05
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2019-03-10 15:14:00 +0530

    iot research: heartbeat_keepalive_tuning mqtt python backend (2019-03-10)

---
+++ app/plugins/handler_20190310_800.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — heartbeat_keepalive_tuning."""
+class BackendMQTTPlugin:
+    plugin_id = "heartbeat_keepalive_tuning_800"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2019-03-10"}
