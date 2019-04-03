d2b4604066cdafb2615a229a015088c8e94e6378
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2019-04-03 21:42:00 +0530

    iot research: coap_mqtt_bridge mqtt python backend (2019-04-03)

---
+++ research/plugins/plugin_2019-04-03_848.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: coap_mqtt_bridge (2019-04-03)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20190403_848 = MQTTPluginHandler("coap_mqtt_bridge", lambda p: p)
