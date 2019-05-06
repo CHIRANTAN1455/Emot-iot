7ebcca45c71331c8e29ae71273b429a47000ac98
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2019-05-06 17:07:00 +0530

    iot research: zigbee2mqtt_integration mqtt python backend (2019-05-06)

---
+++ research/plugins/plugin_2019-05-06_896.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: zigbee2mqtt_integration (2019-05-06)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20190506_896 = MQTTPluginHandler("zigbee2mqtt_integration", lambda p: p)
