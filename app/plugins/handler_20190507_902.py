ecbef5ce1e02651ea029fa6d0191d1f5d05897b5
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2019-05-07 11:07:00 +0530

    iot research: retain_flag_patterns mqtt python backend (2019-05-07)

---
+++ app/plugins/handler_20190507_902.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — retain_flag_patterns."""
+class BackendMQTTPlugin:
+    plugin_id = "retain_flag_patterns_902"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2019-05-07"}
