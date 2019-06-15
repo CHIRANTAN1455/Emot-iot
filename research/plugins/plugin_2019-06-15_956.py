b793bb2e5003c16f0c0a9a05e6179975d962bafe
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2019-06-15 10:51:00 +0530

    iot research: subscribe_pattern_wildcards mqtt python backend (2019-06-15)

---
+++ research/plugins/plugin_2019-06-15_956.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: subscribe_pattern_wildcards (2019-06-15)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20190615_956 = MQTTPluginHandler("subscribe_pattern_wildcards", lambda p: p)
