eec1a26cf73d377ca74be2eeeefc3230f52cdb72
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2019-08-28 19:51:00 +0530

    iot research: subscribe_pattern_wildcards mqtt python backend (2019-08-28)

---
+++ app/plugins/handler_20190828_1094.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — subscribe_pattern_wildcards."""
+class BackendMQTTPlugin:
+    plugin_id = "subscribe_pattern_wildcards_1094"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2019-08-28"}
