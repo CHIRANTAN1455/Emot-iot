3d3cb654b64ca9a480e10e68fced5c259d4571fb
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2020-02-15 16:42:00 +0530

    iot research: actuator_command_queue mqtt python backend (2020-02-15)

---
+++ research/plugins/plugin_2020-02-15_1351.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: actuator_command_queue (2020-02-15)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20200215_1351 = MQTTPluginHandler("actuator_command_queue", lambda p: p)
