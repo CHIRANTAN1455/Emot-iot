44566b002e9f4e75e7d8518f7250f2b0f8659b54
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2020-02-18 14:07:00 +0530

    iot research: json_payload_validation mqtt python backend (2020-02-18)

---
+++ research/plugins/plugin_2020-02-18_1366.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: json_payload_validation (2020-02-18)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20200218_1366 = MQTTPluginHandler("json_payload_validation", lambda p: p)
