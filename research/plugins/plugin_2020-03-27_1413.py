a84fb2961f2b72335d8bdb5c946fcb07ca6d91d5
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2020-03-27 15:42:00 +0530

    iot research: device_shadow_sync mqtt python backend (2020-03-27)

---
+++ research/plugins/plugin_2020-03-27_1413.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: device_shadow_sync (2020-03-27)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20200327_1413 = MQTTPluginHandler("device_shadow_sync", lambda p: p)
