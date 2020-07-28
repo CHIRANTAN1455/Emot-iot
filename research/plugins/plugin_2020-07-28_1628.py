57f705dd27b33e21156b65372d09317350d98825
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2020-07-28 20:31:00 +0530

    iot research: heartbeat_keepalive_tuning mqtt python backend (2020-07-28)

---
+++ research/plugins/plugin_2020-07-28_1628.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: heartbeat_keepalive_tuning (2020-07-28)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20200728_1628 = MQTTPluginHandler("heartbeat_keepalive_tuning", lambda p: p)
