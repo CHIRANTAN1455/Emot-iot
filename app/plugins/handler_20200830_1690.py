7bbf7008dc7132b9043d3af8ce584188a8cc893e
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2020-08-30 19:51:00 +0530

    iot research: edge_gateway_buffer mqtt python backend (2020-08-30)

---
+++ app/plugins/handler_20200830_1690.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — edge_gateway_buffer."""
+class BackendMQTTPlugin:
+    plugin_id = "edge_gateway_buffer_1690"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2020-08-30"}
