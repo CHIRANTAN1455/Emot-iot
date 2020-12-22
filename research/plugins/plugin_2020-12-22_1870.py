a6a324b32f9117303eab33a55caf6f2c799183e2
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2020-12-22 09:51:00 +0530

    iot research: session_persistence mqtt python backend (2020-12-22)

---
+++ research/plugins/plugin_2020-12-22_1870.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: session_persistence (2020-12-22)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20201222_1870 = MQTTPluginHandler("session_persistence", lambda p: p)
