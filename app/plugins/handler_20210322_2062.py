3c799a4993968e490baacff805dcd7c77804262c
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2021-03-22 14:07:00 +0530

    iot research: broker_load_balancing mqtt python backend (2021-03-22)

---
+++ app/plugins/handler_20210322_2062.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — broker_load_balancing."""
+class BackendMQTTPlugin:
+    plugin_id = "broker_load_balancing_2062"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2021-03-22"}
