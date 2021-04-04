39ee6a1f774773f3da2e8c71f6b77024fdbc4ed7
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2021-04-04 13:51:00 +0530

    iot research: retain_flag_patterns mqtt python backend (2021-04-04)

---
+++ research/plugins/plugin_2021-04-04_2075.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: retain_flag_patterns (2021-04-04)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20210404_2075 = MQTTPluginHandler("retain_flag_patterns", lambda p: p)
