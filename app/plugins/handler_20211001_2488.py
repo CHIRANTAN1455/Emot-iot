9c5b4f76974bcc2a697762ce2d504f1d6bec7ef6
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2021-10-01 16:14:00 +0530

    iot research: will_message_handling mqtt python backend (2021-10-01)

---
+++ app/plugins/handler_20211001_2488.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — will_message_handling."""
+class BackendMQTTPlugin:
+    plugin_id = "will_message_handling_2488"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2021-10-01"}
