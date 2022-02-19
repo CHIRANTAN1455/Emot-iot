2bdca87988f839b7a2eeca88414edfa3053cef77
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2022-02-19 13:42:00 +0530

    iot research: actuator_command_queue mqtt python backend (2022-02-19)

---
+++ app/plugins/handler_20220219_2731.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — actuator_command_queue."""
+class BackendMQTTPlugin:
+    plugin_id = "actuator_command_queue_2731"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2022-02-19"}
