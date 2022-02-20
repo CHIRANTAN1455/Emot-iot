a699b21343c7bdeff76288b3a152465a51931c49
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2022-02-20 20:07:00 +0530

    iot research: paho_python_bridge mqtt python backend (2022-02-20)

---
+++ app/plugins/handler_20220220_2738.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — paho_python_bridge."""
+class BackendMQTTPlugin:
+    plugin_id = "paho_python_bridge_2738"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2022-02-20"}
