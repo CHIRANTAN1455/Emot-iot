b20b2aa17f05484ae24ab5b25f7d82d635caf525
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2022-05-16 19:14:00 +0530

    iot research: backend_message_router mqtt python backend (2022-05-16)

---
+++ app/plugins/handler_20220516_2832.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — backend_message_router."""
+class BackendMQTTPlugin:
+    plugin_id = "backend_message_router_2832"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2022-05-16"}
