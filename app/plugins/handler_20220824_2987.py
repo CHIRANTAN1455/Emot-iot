addd5923871826914bd1f58ba0ac3fa906635853
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2022-08-24 15:31:00 +0530

    iot research: coap_mqtt_bridge mqtt python backend (2022-08-24)

---
+++ app/plugins/handler_20220824_2987.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — coap_mqtt_bridge."""
+class BackendMQTTPlugin:
+    plugin_id = "coap_mqtt_bridge_2987"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2022-08-24"}
