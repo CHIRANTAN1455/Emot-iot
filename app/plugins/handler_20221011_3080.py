68d03daeaf3a8bf2a7cf34b29bc0898d78e6164a
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2022-10-11 13:23:00 +0530

    iot research: lorawan_uplink_mqtt mqtt python backend (2022-10-11)

---
+++ app/plugins/handler_20221011_3080.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — lorawan_uplink_mqtt."""
+class BackendMQTTPlugin:
+    plugin_id = "lorawan_uplink_mqtt_3080"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2022-10-11"}
