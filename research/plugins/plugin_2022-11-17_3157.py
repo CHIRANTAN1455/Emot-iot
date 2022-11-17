5cef0e99c5dab69afdefccba6641faece0d22f9e
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2022-11-17 20:31:00 +0530

    iot research: topic_hierarchy_design mqtt python backend (2022-11-17)

---
+++ research/plugins/plugin_2022-11-17_3157.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: topic_hierarchy_design (2022-11-17)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20221117_3157 = MQTTPluginHandler("topic_hierarchy_design", lambda p: p)
