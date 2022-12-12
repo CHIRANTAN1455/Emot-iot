9e3b477bb23aaf6af61b8b7e2f1d922bd393b119
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2022-12-12 17:51:00 +0530

    iot research: tls_mqtt_handshake mqtt python backend (2022-12-12)

---
+++ app/plugins/handler_20221212_3232.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — tls_mqtt_handshake."""
+class BackendMQTTPlugin:
+    plugin_id = "tls_mqtt_handshake_3232"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2022-12-12"}
