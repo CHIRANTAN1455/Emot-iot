60b43cca278c32f91f9570d0f16e4a91927f191c
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2023-02-04 16:51:00 +0530

    iot research: will_message_handling mqtt python backend (2023-02-04)

---
+++ research/plugins/plugin_2023-02-04_3339.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: will_message_handling (2023-02-04)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20230204_3339 = MQTTPluginHandler("will_message_handling", lambda p: p)
