7c479e1b904b90153210c83a7c3eed921672c21d
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2023-04-18 11:23:00 +0530

    iot research: sensor_batch_ingest mqtt python backend (2023-04-18)

---
+++ app/plugins/handler_20230418_3466.py
@@ -0,0 +1,6 @@
+"""FastAPI MQTT backend plugin — sensor_batch_ingest."""
+class BackendMQTTPlugin:
+    plugin_id = "sensor_batch_ingest_3466"
+
+    def handle(self, message: dict) -> dict:
+        return {"plugin": self.plugin_id, "deviceId": message.get("deviceId", "unknown"), "date": "2023-04-18"}
