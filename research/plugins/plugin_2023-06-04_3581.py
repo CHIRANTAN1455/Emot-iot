30b7c904c236a2f396a7b63059c158243a52e852
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2023-06-04 17:14:00 +0530

    iot research: sensor_batch_ingest mqtt python backend (2023-06-04)

---
+++ research/plugins/plugin_2023-06-04_3581.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: sensor_batch_ingest (2023-06-04)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20230604_3581 = MQTTPluginHandler("sensor_batch_ingest", lambda p: p)
