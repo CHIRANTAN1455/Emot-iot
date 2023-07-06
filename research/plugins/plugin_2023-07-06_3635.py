a20991457178641b76925756d6136f42677b3f5b
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2023-07-06 09:51:00 +0530

    iot research: paho_python_bridge mqtt python backend (2023-07-06)

---
+++ research/plugins/plugin_2023-07-06_3635.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: paho_python_bridge (2023-07-06)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20230706_3635 = MQTTPluginHandler("paho_python_bridge", lambda p: p)
