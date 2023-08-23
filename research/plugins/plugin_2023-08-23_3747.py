efdffb9fce5f39f6b1b12368d9a474c31c65d2c4
CHIRANTAN1455 <chirantan.pradhan2019@vitbhopal.ac.in>
2023-08-23 19:51:00 +0530

    iot research: lorawan_uplink_mqtt mqtt python backend (2023-08-23)

---
+++ research/plugins/plugin_2023-08-23_3747.py
@@ -0,0 +1,12 @@
+"""MQTT plugin: lorawan_uplink_mqtt (2023-08-23)."""
+from typing import Any, Callable
+
+class MQTTPluginHandler:
+    def __init__(self, name: str, handler: Callable[[dict], Any]) -> None:
+        self.name = name
+        self._handler = handler
+
+    def on_message(self, topic: str, payload: dict) -> Any:
+        return self._handler({"mqtt_topic": topic, **payload})
+
+handler_20230823_3747 = MQTTPluginHandler("lorawan_uplink_mqtt", lambda p: p)
